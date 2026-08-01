70fd0145e5f3b457f7759ef74b7f287f5cbe3bbe
Erik Bjerke <contact@erikbjerke.com>
2026-08-01 14:03:36 +0100

    UI improvement

--- components/AddToGroupModal.vue
+++ components/AddToGroupModal.vue
@@ -1,48 +1,64 @@
 <template>
   <UiPrompt :show="isOpen" title="Add to Group" @close="$emit('close')">
-    <div class="space-y-1 max-h-60 overflow-y-auto">
+    <div class="flex flex-col gap-1">
       <!-- Create new group option -->
-      <UiButton variant="ghost" color="primary" block @click="$emit('create-new')">
-        <Icon name="mdi:folder-plus-outline" class="w-4.5 h-4.5 flex-shrink-0" />
-        Create new group
-      </UiButton>
-
-      <div v-if="groups.length">
-        <UiDivider color="medium" class="my-2" />
-      </div>
-
-      <!-- Existing groups -->
       <UiButton
-        v-for="group in groups"
-        :key="group.id"
-        variant="ghost"
-        block
-        :class="
-          currentGroupId === group.id
-            ? 'bg-primary-50 dark:bg-primary-900/20 text-primary-700 dark:text-primary-300'
-            : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
-        "
-        @click="$emit('select', group.id)"
+        variant="menu-item"
+        color="primary"
+        class="rounded-lg py-2 text-primary-600 dark:text-primary-400 hover:bg-primary-50 dark:hover:bg-primary-900/20"
+        @click="$emit('create-new')"
       >
-        <Icon
-          name="mdi:folder-outline"
-          class="w-4.5 h-4.5 flex-shrink-0"
-          :class="currentGroupId === group.id ? 'text-primary-500' : 'text-gray-400'"
-        />
-        <span class="truncate">{{ group.name }}</span>
-        <Icon
-          v-if="currentGroupId === group.id"
-          name="mdi:check"
-          class="w-4 h-4 ml-auto text-primary-500 flex-shrink-0"
-        />
+        <Icon name="mdi:folder-plus-outline" class="w-4.5 h-4.5 flex-shrink-0" />
+        <span class="font-medium">Create new group</span>
       </UiButton>
+
+      <template v-if="groups.length">
+        <p
+          class="px-3 pt-3 pb-1 text-[11px] font-semibold uppercase tracking-wider text-gray-400 dark:text-gray-500 select-none"
+        >
+          Your groups
+        </p>
+
+        <!-- Existing groups -->
+        <div class="max-h-64 -mx-1 overflow-y-auto px-1">
+          <UiButton
+            v-for="group in groups"
+            :key="group.id"
+            variant="menu-item"
+            class="rounded-lg py-2"
+            :class="
+              currentGroupId === group.id
+                ? 'bg-primary-50 dark:bg-primary-900/20 text-primary-700 dark:text-primary-300 hover:bg-primary-100 dark:hover:bg-primary-900/30'
+                : 'text-gray-700 dark:text-gray-300'
+            "
+            @click="$emit('select', group.id)"
+          >
+            <Icon
+              name="mdi:folder-outline"
+              class="w-4.5 h-4.5 flex-shrink-0"
+              :class="currentGroupId === group.id ? 'text-primary-500' : 'text-gray-400'"
+            />
+            <span class="truncate">{{ group.name }}</span>
+            <Icon
+              v-if="currentGroupId === group.id"
+              name="mdi:check"
+              class="w-4 h-4 ml-auto text-primary-500 flex-shrink-0"
+            />
+          </UiButton>
+        </div>
+      </template>
 
       <!-- Remove from group option -->
       <template v-if="currentGroupId">
-        <UiDivider color="medium" class="my-2" />
-        <UiButton variant="ghost" color="gray" block @click="$emit('select', null)">
+        <UiDivider color="medium" class="my-1" />
+        <UiButton
+          variant="menu-item"
+          color="red"
+          class="rounded-lg py-2"
+          @click="$emit('select', null)"
+        >
           <Icon name="mdi:folder-remove-outline" class="w-4.5 h-4.5 flex-shrink-0" />
-          Remove from group
+          <span>Remove from group</span>
         </UiButton>
       </template>
     </div>
